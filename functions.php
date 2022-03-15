7393ab9cf59ab454f8ccb11052c6738e129fbe23
Alex Cuthbert <69787227+alexcuthbert@users.noreply.github.com>
2022-03-15 17:05:40 +0000

    Footer and fixing header to BS5

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 
 function start_practice02_scripts() {
-	wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+	//wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
 	wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
 	//wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
 }
--- header.php
+++ header.php
@@ -1,50 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <meta name="author" content="Derby & Burton Hospitals Choir">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-    <meta name="author" content="Derby & Burton Hospitals Choir">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  <link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri() . '/style.css' ?>">
 
-	<?php wp_head(); ?>
-	
+  <?php wp_head(); ?>
 </head>
+
 <body>
-
-<header class="">
-	
-<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white p-4">
-  <div class="container">
-
-
-  <div class="d-inline-flex justify-content-start align-items-center">
-    <a href="<?= get_bloginfo('url'); ?>">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/cropped-Choir-Logo-with-Title-1.png" class="navbar-brand logo">
-    </a>
-    <div class="mob-hide">
-      <a href="<?= get_bloginfo('url'); ?>" class="brand">
-        <h1><?= get_bloginfo('name'); ?></h1>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top p-4">
+    <div class="container">
+      <a href="<?= get_bloginfo('url'); ?>">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/cropped-Choir-Logo-with-Title-1.png" class="navbar-brand logo">
       </a>
-      <p><?= get_bloginfo('description'); ?></p>
-    </div>
-  </div>
+      <div class="mob-hide">
+        <a class="navbar-brand" href="<?= get_bloginfo('url'); ?>">
+          <h1><?= get_bloginfo('name'); ?></h1>
+        </a>
+        <p><?= get_bloginfo('description'); ?></p>
+      </div>
 
-  <div class="d-inline-flex justify-content-end">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-  </div>
 
-  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <?php wp_list_pages( '&title_li=' ); ?>
-    </ul>
-  </div>
-
-
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <?php wp_list_pages( '&title_li=' ); ?>
+            </ul>
+          </div>
 </div>
 </nav>
+
+
+
+
+
+
+
+  
+    
+    
+
 
 
