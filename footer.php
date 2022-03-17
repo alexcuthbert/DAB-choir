853c8a718fddc31637b605a516f26255c56eb4df
Alex Cuthbert <69787227+alexcuthbert@users.noreply.github.com>
2022-03-17 20:41:43 +0000

    rename theme

--- footer.php
+++ footer.php
@@ -2,16 +2,32 @@
 
 
 
-      <footer class="container">
-          <?php wp_footer(); ?>
+      <footer class="container p-0 py-3">
+        <?php wp_footer(); ?>
+
+        <?php if ( has_nav_menu( 'primary' ) ) : ?> 
+          <nav class="navbar navbar-expand justify-content-center">
+          
+            
+
+            <?php
+              wp_nav_menu(
+                array(
+                  'theme_location'  => 'footer',
+                  'menu_class'      => 'menu-wrapper',
+                  'container_class' => 'footer-menu-container',
+                  'items_wrap'      => '<ul class="navbar-nav mb-2 mb-lg-0">%3$s</ul>',
+                  'fallback_cb'     => false,
+                )
+              );
+              ?>            
+          </nav>
+        <?php endif; ?>
         <div class="d-flex justify-content-center my-5-lg mx-3">
             <p>Derby and Burton Hospitals Choir &#169; 2022 | Site design by <a href="http://alexdavidcuthbert.co.uk">Alex David Cuthbert</a></p>
         </div>
 
       </footer>
-
-        <!-- Optional JavaScript; choose one of the two! -->
-
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
